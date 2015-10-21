89a5ab8a06af90322c003f130a5a451d76dcfe37
Taras Pasichnyk <taras.pasichnyk@kindgeek.com>
2015-10-21 23:53:58 +0300

    added cell in Feed

--- MyPersonalTrainer/app/src/main/java/com/greenlionsteam/mypersonaltrainer/MainFeedActivity.java
+++ MyPersonalTrainer/app/src/main/java/com/greenlionsteam/mypersonaltrainer/MainFeedActivity.java
@@ -58,13 +58,22 @@ public class MainFeedActivity extends AppCompatActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = getString(R.string.title_schedule);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                mTitle = getString(R.string.title_calendar);
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+                mTitle = getString(R.string.title_parameters);
+                break;
+            case 4:
+                mTitle = getString(R.string.title_statistics);
+                break;
+            case 5:
+                mTitle = getString(R.string.title_music);
+                break;
+            case 6:
+                mTitle = getString(R.string.title_videos);
                 break;
         }
     }
