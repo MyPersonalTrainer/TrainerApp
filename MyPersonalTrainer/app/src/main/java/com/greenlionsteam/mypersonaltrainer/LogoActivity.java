ddb9d6cfd50eaec0e79691a02f3136c0af08da59
bohdanuhryn <bohdanuhryn@gmail.com>
2015-11-29 21:37:47 +0200

    logo screen added

---
+++ MyPersonalTrainer/app/src/main/java/com/greenlionsteam/mypersonaltrainer/LogoActivity.java
@@ -0,0 +1,53 @@
+package com.greenlionsteam.mypersonaltrainer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by BohdanUhryn on 29.11.2015.
+ */
+public class LogoActivity extends AppCompatActivity {
+
+    private ImageView logoView;
+    private Animation logoAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_logo);
+        initViews();
+        startLogoAnimation();
+    }
+
+    private void initViews() {
+        logoView = (ImageView) findViewById(R.id.logo_view);
+    }
+
+    private void startLogoAnimation() {
+        logoAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_logo);
+        logoAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startMainMenuActivity();
+            }
+
+            @Override public void onAnimationRepeat(Animation animation) {}
+        });
+        logoView.startAnimation(logoAnimation);
+    }
+
+    private void startMainMenuActivity() {
+        Intent intent = new Intent(this, MainFeedActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+}
